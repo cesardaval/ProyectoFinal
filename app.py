826d16cb2f64b349d5c332f5a354fe0cd5a0d1f0
Msteroz <cesarhquinterod@gmail.com>
2020-01-26 08:04:17 -0400

    solo me dio chance para hacer funcionar el registro :V amor falta adarle estilo al campo de email :V que sale simple sin ningun estilo <3

--- app.py
+++ app.py
@@ -18,36 +18,57 @@ def inicio():
         return "estas logeado"
     return "inicio"
 
+
 @app.route('/index')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route('/mision')
 def mision():
-	return render_template("mision.html")
+    return render_template("mision.html")
+
 
 @app.route('/historia')
 def historia():
-	return render_template("historia.html")
+    return render_template("historia.html")
 
-@app.route('/registro')
+
+@app.route('/registro', methods=['GET', 'POST'])
 def registro():
-    form = forms.Formulario()
-    return render_template("registro.html",forms = form)
+    """
+    esta ruta da el formulario de registro.
+
+    """
+    # objeto formulario para pasarlo a jinja2
+    form = forms.Formulario(request.form)
+    # si la respuesta del formulario es de tipo post y si no tiene errores
+    # entonces crea una instancia Users y rellena para agregarlo a la base
+    # la tabla Users
+    if request.method == 'POST' and form.validate():
+        user = User(form.nombre.data, form.cedula.data,
+                    form.username.data, form.email.data,
+                    form.password.data)
+        db.session.add(user)
+        db.session.commit()
+    return render_template("registro.html", forms=form)
+
 
 @app.route('/reporte')
 def reporte():
 
-    item = [items("Maria", "correo@yahoo.com", "loca", "1"),]
+    item = [items("Maria", "correo@yahoo.com", "loca", "1"), ]
     tabla = Tabla(item)
-    return render_template("reporte.html", tabla = tabla)
+    return render_template("reporte.html", tabla=tabla)
 
 
 @app.route('/preinscripcion/<string:usuario>')
 def preinscripcion(usuario="cesar"):
+    hola = usuario
+    print(hola)
     return render_template("perfil.html")
 
-    
+
 if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
